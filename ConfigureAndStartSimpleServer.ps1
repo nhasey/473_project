b9cb1f667e16f2c32c5ed39880cbdc972707325e
nhasey <157612356+nhasey@users.noreply.github.com>
2024-10-28 14:16:36 -0400

    added powershell scripts to help start server

---
+++ ConfigureAndStartSimpleServer.ps1
@@ -0,0 +1,5 @@
+npm init -y
+npm install express mongodb body-parser ejs
+node server.js
+
+. .\StartSimpleServer.ps1
---
+++ StartSimpleServer.ps1
@@ -0,0 +1,2 @@
+Start-Process chrome.exe '--new-window http://localhost:3000/'
+node server.js
